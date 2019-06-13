1c3b40b19608f752036decc60df9565c30dd452f
chetan882777 <chetan882777@gmail.com>
2019-06-13 15:28:24 +0530

    use html file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
+const path = require('path')
 const express= require('express')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname , "../public")
 
+app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-    res.send('<h1>Chat App</h1>')
-})
-
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up on port ' + port)
 })
