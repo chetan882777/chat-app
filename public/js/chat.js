0b11cddadcaf6b569b2c851c073edce7fe1ae15b
chetan882777 <chetan882777@gmail.com>
2019-06-13 21:39:13 +0530

    Event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
-const socket = io() 
+const socket = io()
 
-socket.on('message', (message) =>{
+socket.on('message', (message) => {
     console.log(message)
 })
 
@@ -8,18 +8,24 @@ socket.on('message', (message) =>{
 document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit('sendMessage' , message)
+    socket.emit('sendMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click', (e) => {
-    if(!navigator.geolocation){
+    if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser!')
     }
 
-    navigator.geolocation.getCurrentPosition((position)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
         const currentLocation = {
-            latitude : position.coords.latitude,
-            longitude : position.coords.longitude
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
         }
 
         console.log(currentLocation)
